f90d96d7869633d3f47c6ad9ec055fe3fa583da1
Hanbings <3219065882@qq.com>
2020-07-29 13:19:44 +0800

    :construction:​ Feat: MySQL working
    Feat: MySQL working

---
+++ src/main/java/com/rabbitown/yalib/test/TestMySQL.java
@@ -0,0 +1,13 @@
+package com.rabbitown.yalib.test;
+
+import com.rabbitown.yalib.database.MySQL;
+
+/**
+ * 测试类 可随时删除
+ * @author Hanbings
+ */
+public class TestMySQL {
+    public static void main(String[] args) {
+        MySQL mySQL = new MySQL();
+    }
+}
