e401b33a2a26e8bb5da0f5ca533511100d428719
Hanbings <3219065882@qq.com>
2020-10-17 15:43:59 +0800

    :sparkles:​ Feat: SQLiteService

---
+++ src/main/java/com/rabbitown/yalib/database/impl/SQLiteDataBaseImpl.java
@@ -0,0 +1,40 @@
+package com.rabbitown.yalib.database.impl;
+
+import com.rabbitown.yalib.database.DataBaseService;
+import org.bukkit.Bukkit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteDataBaseImpl implements DataBaseService {
+    @Override
+    public Connection loadDatabase(String dataBaseName) {
+        Connection connection;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dataBaseName);
+            Bukkit.getServer().getLogger().info("[SQLITE][INFO] 数据库加载成功");
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            Bukkit.getServer().getLogger().warning("[SQLITE][ERROR] 连接数据库发生错误");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void execute(Connection connection, String sql) {
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+            connection.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
