e4cf4705914b885097e08ddec3cf087ab5d2dc13
Yoooooory <youranpublic@gmail.com>
2020-11-28 20:45:20 +0800

    :art: Style: sort code

--- src/main/java/com/rabbitown/yalib/YaLibCenter.kt
+++ src/main/java/com/rabbitown/yalib/YaLibCenter.kt
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin
  */
 object YaLibCenter {
 
-    val pluginMap = mutableMapOf<String, JavaPlugin>("com.rabbitown.yalib" to YaLib.instance)
+    val pluginMap = mutableMapOf<String, JavaPlugin>()
 
     fun registerPlugin(prefix: String, plugin: JavaPlugin) {
         pluginMap[prefix] = plugin
