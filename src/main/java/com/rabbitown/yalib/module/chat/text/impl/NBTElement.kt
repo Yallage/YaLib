b67dec83381be6d4bc762e9f5fd4774de17c3ada
Yoooooory <youranpublic@gmail.com>
2021-02-02 12:48:04 +0800

    :bug: Fix: small bug

--- src/main/java/com/rabbitown/yalib/module/chat/text/impl/NBTElement.kt
+++ src/main/java/com/rabbitown/yalib/module/chat/text/impl/NBTElement.kt
@@ -5,7 +5,7 @@ import com.rabbitown.yalib.module.chat.text.JSONTextElement
 /**
  * @author Yoooooory
  */
-internal open class NBTElement(val nbt: String, val interpret: String) : JSONTextElement() {
+sealed class NBTElement(val nbt: String, val interpret: String) : JSONTextElement() {
     class Block(nbt: String, interpret: String, val block: String) : NBTElement(nbt, interpret)
     class Entity(nbt: String, interpret: String, val entity: String) : NBTElement(nbt, interpret)
     class Storage(nbt: String, interpret: String, val storage: String) : NBTElement(nbt, interpret)
